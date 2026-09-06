18054fc702c1ce44fc54358d6b19766b7e3bf87d
Julien Eudine <julien@eudine.fr>
2026-09-06 23:39:13 +0200

    Initial NixOS config for server

---
+++ configuration.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ ./hardware-configuration.nix ];
+
+  # Bootloader (UEFI)
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Hostname and time
+  networking.hostName = "server";
+  time.timeZone = "Europe/Zurich";
+
+  users.users.julien = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+  };
+
+  # SSH
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = false;
+  };
+
+  environment.systemPackages = with pkgs; [ git vim htop ];
+
+  system.stateVersion = "26.05";
+}
